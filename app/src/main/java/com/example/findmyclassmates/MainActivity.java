436792087396ed63713ff5698f32244db54e14c3
Nikki Yaminrafie <yaminraf@usc.edu>
2023-11-03 22:57:08 -0700

    Landing Page UI Created

--- app/src/main/java/com/example/findmyclassmates/MainActivity.java
+++ app/src/main/java/com/example/findmyclassmates/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.findmyclassmates;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button signInButton, signUpButton;
+    EditText usernameEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the buttons
+        signInButton = findViewById(R.id.signInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        // Initialize the EditTexts
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Capture user input from EditTexts
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Validate input
+                if (validateInput(username, password)) {
+                    // Here you would have your sign-in logic
+                    signIn(username, password);
+                }
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to your sign-up activity or logic
+                signUp();
+            }
+        });
+    }
+
+    private boolean validateInput(String username, String password) {
+        // Here you would check for validity, e.g., non-empty fields
+        if (username.isEmpty()) {
+            usernameEditText.setError("Username cannot be empty");
+            return false;
+        }
+        if (password.isEmpty()) {
+            passwordEditText.setError("Password cannot be empty");
+            return false;
+        }
+        // Add other validation checks as necessary
+        return true;
+    }
+
+    private void signIn(String username, String password) {
+        // Replace this with your actual sign-in logic.
+        // Currently, just showing a Toast message.
+        Toast.makeText(MainActivity.this, "Signing in with Username: " + username, Toast.LENGTH_LONG).show();
+        // Intent to another activity could go here if sign in is successful
+    }
+
+    private void signUp() {
+        // Replace this with your actual sign-up navigation or logic.
+        // Currently, just showing a Toast message.
+        Toast.makeText(MainActivity.this, "Navigate to Sign Up", Toast.LENGTH_LONG).show();
+        // Intent to another activity could go here to handle sign up
     }
 }
