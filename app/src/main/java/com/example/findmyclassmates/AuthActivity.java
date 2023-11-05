9292dc8ac220b692963964d67f240d42ab1af62f
Seena Pourzand <seenapourzand@gmail.com>
2023-11-05 15:08:40 -0800

    not working commit, just to track progress

--- app/src/main/java/com/example/findmyclassmates/AuthActivity.java
+++ app/src/main/java/com/example/findmyclassmates/AuthActivity.java
@@ -2,20 +2,33 @@ package com.example.findmyclassmates;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+// firebase imports
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AuthActivity extends AppCompatActivity {
 
     Button signInButton, signUpButton;
     EditText usernameEditText, passwordEditText;
     BottomNavigationView bottomNavigationView;
+
+    // firebase
+    DatabaseReference dbReference;
+    FirebaseDatabase fbRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +79,42 @@ public class AuthActivity extends AppCompatActivity {
 
     private void signIn(String username, String password) {
         // Replace this with your actual sign-in logic.
-        // If sign-in is successful:
+        Log.println(Log.INFO,"Inside signIn", "usernameGiven: " + username + ", and passwordGiven: " + password);
+
+        // If input is valid:
         if (validateInput(username, password)) {
-            Intent mainActivityIntent = new Intent(AuthActivity.this, MainActivity.class);
-            startActivity(mainActivityIntent);
-            finish(); // Call this to finish the current Activity and remove it from the back stack.
+            boolean correctCredentials = false;
+            fbRoot = FirebaseDatabase.getInstance();
+
+            // need to do error checking to see what could go wrong if invalid username
+            dbReference = fbRoot.getReference(username);
+            // obtain user's password
+            Task<com.google.firebase.database.DataSnapshot> retrievedPassObject = dbReference.get();
+            String retrievedPass = retrievedPassObject.getResult().getValue().toString();
+            Log.println(Log.INFO,"Inside signIn", "retrievedPass: " + retrievedPass);
+
+
+            if(retrievedPass.equals(password)) {
+                // continue with log in flow
+                Log.println(Log.INFO,"Inside signIn comparison", "we should be good");
+
+                correctCredentials = true;
+
+                // destroy when done i guess
+                fbRoot = null;
+                dbReference = null;
+            } else {
+                // cannot proceed
+
+            }
+
+            if(correctCredentials) {
+                Intent mainActivityIntent = new Intent(AuthActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+                finish(); // Call this to finish the current Activity and remove it from the back stack.
+            } else {
+                //indicate to user somehow the password is incorrect for that user
+            }
         }
     }
 
