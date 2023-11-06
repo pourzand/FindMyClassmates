47771fe28f96b0d74c74277fcbf763ee074a7192
Diya Pathak <dpathak@usc.edu>
2023-11-06 15:47:59 -0800

    Goes back and forth, but still crashes when getting the response

--- app/src/main/java/com/example/findmyclassmates/RateClassFragment.java
+++ app/src/main/java/com/example/findmyclassmates/RateClassFragment.java
@@ -1,50 +1,58 @@
 package com.example.findmyclassmates;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class RateClassFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class RateClassFragment extends Fragment implements RatingFormFragment.RatingFormListener {
+
+    private TextView classNameTextView;
+    private StringBuilder concatenatedResponses = new StringBuilder();
+    private ClassModel selectedClass;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rate_class, container, false);
+
+        classNameTextView = view.findViewById(R.id.classNameTextView);
+
+        if (getArguments() != null) {
+            selectedClass = getArguments().getParcelable("selectedClass");
+            classNameTextView.setText("Ratings for : " + selectedClass.getClassName());
+        }
 
         Button buttonAddRating = view.findViewById(R.id.buttonAddRating);
         buttonAddRating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Assuming you have a RatingFormFragment
                 RatingFormFragment ratingFormFragment = new RatingFormFragment();
-                // Optionally pass any arguments to the RatingFormFragment
 
-                // Replace the current fragment with RatingFormFragment
                 if (getFragmentManager() != null) {
                     getFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, ratingFormFragment) // Replace 'fragment_container' with your actual container ID
-                            .addToBackStack(null) // Add transaction to back stack to enable back navigation
+                            .replace(R.id.fragment_container, ratingFormFragment)
+                            .addToBackStack(null)
                             .commit();
                 }
             }
         });
 
-        ImageView backButton = view.findViewById(R.id.backButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle what happens when the back button is clicked
-                // Typically, we just pop the back stack
-                if (getFragmentManager() != null) {
-                    getFragmentManager().popBackStack();
-                }
-            }
-        });
+        TextView responseTextView = view.findViewById(R.id.responseTextView);
+        responseTextView.setText(concatenatedResponses.toString());
 
         return view;
+    }
+
+    @Override
+    public void onRatingFormSubmit(String concatenatedResponses) {
+        this.concatenatedResponses.append(concatenatedResponses).append("\n\n");
+        TextView responseTextView = getView().findViewById(R.id.responseTextView);
+        responseTextView.setText(this.concatenatedResponses.toString());
     }
 }
--- app/src/main/java/com/example/findmyclassmates/RatingFormFragment.java
+++ app/src/main/java/com/example/findmyclassmates/RatingFormFragment.java
@@ -1,45 +1,82 @@
 package com.example.findmyclassmates;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class RatingFormFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
+    private EditText prompt1EditText;
+    private EditText prompt2EditText;
+    private EditText prompt3EditText;
+    private EditText prompt4EditText;
+    private EditText prompt5EditText;
+    private Spinner sixthRatingSpinner;
+    private RatingFormListener mListener;
 
-    public RatingFormFragment() {
-        // Required empty public constructor
-    }
-
-    public static RatingFormFragment newInstance(String param1, String param2) {
-        RatingFormFragment fragment = new RatingFormFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public interface RatingFormListener {
+        void onRatingFormSubmit(String concatenatedResponses);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof RatingFormListener) {
+            mListener = (RatingFormListener) context;
         }
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rating_form, container, false);
+
+        prompt1EditText = view.findViewById(R.id.prompt1EditText);
+        prompt2EditText = view.findViewById(R.id.prompt2EditText);
+        prompt3EditText = view.findViewById(R.id.prompt3EditText);
+        prompt4EditText = view.findViewById(R.id.prompt4EditText);
+        prompt5EditText = view.findViewById(R.id.prompt5EditText);
+        sixthRatingSpinner = view.findViewById(R.id.sixthRatingSpinner);
+
+        Button submitButton = view.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateForm()) {
+                    String response1 = prompt1EditText.getText().toString();
+                    String response2 = prompt2EditText.getText().toString();
+                    String response3 = prompt3EditText.getText().toString();
+                    String response4 = prompt4EditText.getText().toString();
+                    String response5 = prompt5EditText.getText().toString();
+                    String rating = sixthRatingSpinner.getSelectedItem().toString();
+
+                    String concatenatedResponses =
+                            "1. The workload of the class: " + response1 + "\n" +
+                                    "2. The score they would like to give to this class: " + response2 + "\n" +
+                                    "3. If the professor checks attendance: " + response3 + "\n" +
+                                    "4. If the professor allows late homework submission: " + response4 + "\n" +
+                                    "5. Other comments: " + response5 + "\n" +
+                                    "6. Rating: " + rating;
+
+                    mListener.onRatingFormSubmit(concatenatedResponses);
+
+                    if (getFragmentManager() != null) {
+                        getFragmentManager().popBackStack();
+                    }
+                } else {
+                    Toast.makeText(getContext(), "Please fill out all prompts", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         ImageView backButton = view.findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -52,5 +89,16 @@ public class RatingFormFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private boolean validateForm() {
+        String response1 = prompt1EditText.getText().toString();
+        String response2 = prompt2EditText.getText().toString();
+        String response3 = prompt3EditText.getText().toString();
+        String response4 = prompt4EditText.getText().toString();
+        String response5 = prompt5EditText.getText().toString();
+
+        return !response1.isEmpty() && !response2.isEmpty() && !response3.isEmpty()
+                && !response4.isEmpty() && !response5.isEmpty();
     }
 }
