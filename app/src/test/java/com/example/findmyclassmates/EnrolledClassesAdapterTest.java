09c540a43f72b49201d355730860c7d2860fc884
Nikki Yaminrafie <yaminraf@usc.edu>
2023-11-21 22:50:24 -0800

    Diya extra passing test

--- app/src/test/java/com/example/findmyclassmates/EnrolledClassesAdapterTest.java
+++ app/src/test/java/com/example/findmyclassmates/EnrolledClassesAdapterTest.java
@@ -10,6 +10,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.widget.TextView;
 
 public class EnrolledClassesAdapterTest {
 
@@ -19,8 +24,14 @@ public class EnrolledClassesAdapterTest {
     private String expectedClassName;
     private List<String> expectedStudents;
 
+    @Mock
+    private TextView mockTextView;
+    @Mock
+    private RecyclerView mockRecyclerView;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         // Set up for ClassData
         expectedClassName = "Test Class";
         expectedStudents = Arrays.asList("Alice", "Bob", "Charlie");
@@ -50,5 +61,23 @@ public class EnrolledClassesAdapterTest {
     public void adapter_itemCount_returnsCorrectCount() {
         assertEquals(2, adapter.getItemCount());
     }
-}
 
+    @Test
+    public void onBindViewHolder_CalledMultipleTimes_VerifyMethodCalls() {
+        EnrolledClassesAdapter.ClassViewHolder mockViewHolder = mock(EnrolledClassesAdapter.ClassViewHolder.class);
+        mockViewHolder.classTextView = mockTextView;
+        mockViewHolder.studentsRecyclerView = mockRecyclerView;
+
+        // Simulate onBindViewHolder being called 3 times
+        adapter.onBindViewHolder(mockViewHolder, 0);
+        adapter.onBindViewHolder(mockViewHolder, 0);
+        adapter.onBindViewHolder(mockViewHolder, 0);
+
+        // Verify that setText is called exactly 3 times
+        verify(mockTextView, times(3)).setText(expectedClassName);
+
+        // Verify that setAdapter and setLayoutManager are called exactly 3 times each
+        verify(mockRecyclerView, times(3)).setAdapter(any(StudentsAdapter.class));
+        verify(mockRecyclerView, times(3)).setLayoutManager(any(LinearLayoutManager.class));
+    }
+}
