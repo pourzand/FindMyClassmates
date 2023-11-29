843ec774fb9697ea198f205dd2c5975e82e27cd2
Nikki Yaminrafie <yaminraf@usc.edu>
2023-11-28 22:42:04 -0800

    upvotes/downvotes

--- app/src/main/java/com/example/findmyclassmates/RatingAdapter.java
+++ app/src/main/java/com/example/findmyclassmates/RatingAdapter.java
@@ -3,6 +3,7 @@ package com.example.findmyclassmates;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class RatingAdapter extends RecyclerView.Adapter<RatingAdapter.ViewHolder> {
 
-    private List<RatingData> ratings; // Change the type if needed
+    private List<RatingData> ratings;
 
     public void setRatings(List<RatingData> ratings) {
         this.ratings = ratings;
@@ -31,6 +32,56 @@ public class RatingAdapter extends RecyclerView.Adapter<RatingAdapter.ViewHolder
         RatingData rating = ratings.get(position);
         holder.ratingTextView.setText(rating.getRating());
         holder.usernameTextView.setText("User: " + rating.getUsername());
+        holder.upvoteCountTextView.setText(String.valueOf(rating.getUpvotes()));
+        holder.downvoteCountTextView.setText(String.valueOf(rating.getDownvotes()));
+
+        // Upvote logic
+        holder.upvoteIcon.setOnClickListener(v -> handleUpvote(holder, position));
+
+        // Downvote logic
+        holder.downvoteIcon.setOnClickListener(v -> handleDownvote(holder, position));
+    }
+
+    private void handleUpvote(ViewHolder holder, int position) {
+        int currentPosition = holder.getAdapterPosition();
+        if (currentPosition != RecyclerView.NO_POSITION) {
+            RatingData currentRating = ratings.get(currentPosition);
+
+            // Toggle upvote
+            if (currentRating.isUpvotedByCurrentUser()) {
+                currentRating.setUpvotes(currentRating.getUpvotes() - 1);
+                currentRating.setUpvotedByCurrentUser(false);
+            } else {
+                currentRating.setUpvotes(currentRating.getUpvotes() + 1);
+                currentRating.setUpvotedByCurrentUser(true);
+            }
+            holder.upvoteCountTextView.setText(String.valueOf(currentRating.getUpvotes()));
+            updateRatingInDatabase(currentRating, currentPosition);
+        }
+    }
+
+    private void handleDownvote(ViewHolder holder, int position) {
+        int currentPosition = holder.getAdapterPosition();
+        if (currentPosition != RecyclerView.NO_POSITION) {
+            RatingData currentRating = ratings.get(currentPosition);
+
+            // Toggle downvote
+            if (currentRating.isDownvotedByCurrentUser()) {
+                currentRating.setDownvotes(currentRating.getDownvotes() - 1);
+                currentRating.setDownvotedByCurrentUser(false);
+            } else {
+                currentRating.setDownvotes(currentRating.getDownvotes() + 1);
+                currentRating.setDownvotedByCurrentUser(true);
+            }
+            holder.downvoteCountTextView.setText(String.valueOf(currentRating.getDownvotes()));
+            updateRatingInDatabase(currentRating, currentPosition);
+        }
+    }
+
+    private void updateRatingInDatabase(RatingData rating, int position) {
+        // Here you'll write logic to update the rating's upvote and downvote count in the database
+        // After updating in Firebase, you should refresh the specific item
+        // For example: notifyItemChanged(position);
     }
 
     @Override
@@ -41,11 +92,19 @@ public class RatingAdapter extends RecyclerView.Adapter<RatingAdapter.ViewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView ratingTextView;
         TextView usernameTextView;
+        ImageView upvoteIcon;
+        TextView upvoteCountTextView;
+        ImageView downvoteIcon; // New ImageView for downvote
+        TextView downvoteCountTextView; // New TextView for downvote count
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ratingTextView = itemView.findViewById(R.id.ratingTextView);
             usernameTextView = itemView.findViewById(R.id.usernameTextView);
+            upvoteIcon = itemView.findViewById(R.id.upvoteIcon);
+            upvoteCountTextView = itemView.findViewById(R.id.upvoteCountTextView);
+            downvoteIcon = itemView.findViewById(R.id.downvoteIcon); // Initialize the downvote ImageView
+            downvoteCountTextView = itemView.findViewById(R.id.downvoteCountTextView); // Initialize the downvote count TextView
         }
     }
 }
--- app/src/main/java/com/example/findmyclassmates/RatingData.java
+++ app/src/main/java/com/example/findmyclassmates/RatingData.java
@@ -1,13 +1,38 @@
 package com.example.findmyclassmates;
+
 public class RatingData {
     private String username;
     private String rating;
+    private int upvotes;
+    private boolean isUpvotedByCurrentUser;
+    private int downvotes; // New field for downvotes
+    private boolean isDownvotedByCurrentUser; // New field to track if current user downvoted
 
     public RatingData(String username, String rating) {
         this.username = username;
         this.rating = rating;
+        this.upvotes = 0;
+        this.downvotes = 0;
     }
 
+    // Add getter and setter for upvotes
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public int getDownvotes() {
+        return downvotes;
+    }
+
+    public void setDownvotes(int downvotes) {
+        this.downvotes = downvotes;
+    }
+
+    // Existing getters
     public String getUsername() {
         return username;
     }
@@ -15,5 +40,20 @@ public class RatingData {
     public String getRating() {
         return rating;
     }
-}
 
+    public boolean isUpvotedByCurrentUser() {
+        return isUpvotedByCurrentUser;
+    }
+
+    public boolean isDownvotedByCurrentUser() {
+        return isDownvotedByCurrentUser;
+    }
+
+    public void setUpvotedByCurrentUser(boolean upvotedByCurrentUser) {
+        isUpvotedByCurrentUser = upvotedByCurrentUser;
+    }
+
+    public void setDownvotedByCurrentUser(boolean downvotedByCurrentUser) {
+        isDownvotedByCurrentUser = downvotedByCurrentUser;
+    }
+}
